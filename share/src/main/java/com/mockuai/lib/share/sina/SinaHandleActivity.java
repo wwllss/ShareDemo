febf7a413700a39f954ad2492b724df9a5653b3e
zhangyuan <zhangyuan@mockuai.com>
2016-04-01 15:59:04 +0800

    取消SupportV4包依赖。

--- share/src/main/java/com/mockuai/lib/share/sina/SinaHandleActivity.java
+++ share/src/main/java/com/mockuai/lib/share/sina/SinaHandleActivity.java
@@ -1,16 +1,14 @@
 package com.mockuai.lib.share.sina;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Toast;
 
 import com.mockuai.lib.share.IShare;
 import com.mockuai.lib.share.PlatformConfig;
-import com.mockuai.lib.share.R;
 import com.mockuai.lib.share.constant.Platform;
 import com.mockuai.lib.share.factory.PlatformFactory;
 import com.mockuai.lib.share.listener.CallbackManager;
@@ -25,7 +23,7 @@ import com.sina.weibo.sdk.constant.WBConstants;
 /**
  * Created by zhangyuan on 16/3/23.
  */
-public class SinaHandleActivity extends FragmentActivity implements IWeiboHandler.Response {
+public class SinaHandleActivity extends Activity implements IWeiboHandler.Response {
 
     private static final String KEY_TRANSACTION = "TRANSACTION";
 
@@ -43,9 +41,6 @@ public class SinaHandleActivity extends FragmentActivity implements IWeiboHandle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = new View(this);
-        view.setBackgroundColor(getResources().getColor(android.R.color.transparent));
-        setContentView(view);
         api = WeiboShareSDK.createWeiboAPI(this, PlatformConfig.getInstance().getSinaConfig().getAppKey());
         api.registerApp();
         String transaction = getIntent().getStringExtra(KEY_TRANSACTION);
